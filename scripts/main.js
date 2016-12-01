70cebcda6cf44e3b18332e7ac20a1188bb3225c9
hashimojo <hashimojosuzu0810@gmail.com>
2016-12-01 09:40:39 +0900

    11/24

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+function play(){
+    let player = document.querySelector("audio");
+    player.play();
+}
+
+function pause(){
+    let player = document.querySelector("audio");
+    player.pause();
+}
+
+function rewind(sec){
+    let player = document.querySelector("audio");
+    player.currentTime = player.currentTime - sec;
+    return sec;
+}
+
+function fastForward(sec){
+    let player = document.querySelector("audio");
+    player.currentTime = player.currentTime + sec;
+    return sec;
+}
+
+let playButton = document.querySelector("#play-button");
+playButton.addEventListener("click", play);
+
+let pauseButton = document.querySelector("#pause-button");
+pauseButton.addEventListener("click", pause);
+
+let fastForwardButton = document.querySelector("#fast-button");
+fastForwardButton.addEventListener("click", fastForward;
